9f48fc68bbfb2c860616dd3cafecba05b9822412
IzzyGoal <gabriel.osilvalima@gmail.com>
2022-11-06 23:55:28 -0300

    first commit

---
+++ SeguroWebApi/Domain/Cliente.cs
@@ -0,0 +1,27 @@
+﻿namespace SeguroWebApi.Domain
+{
+    public class Cliente
+    {
+        public int id { get; set; }
+
+        public string nome { get; set; }
+
+        public string cpf { get; set; }
+
+        public string rg { get; set; }
+
+        public string endereco { get; set; }
+
+        public string valorPremiado { get; set; }
+
+        public string valorImovel { get; set; }
+
+        public string stPagamentos { get; set; }
+
+        public DateOnly dtInicio { get; set; }
+
+        public DateOnly dtFim { get; set; }
+
+
+    }
+}
---
+++ SeguroWebApi/Domain/ClienteContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SeguroWebApi.Domain
+{
+    public class ClienteContext : DbContext
+    {
+        public ClienteContext(DbContextOptions<ClienteContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
